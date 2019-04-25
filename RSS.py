fcebd098487bf659daf234358a2950281021f084
2doubledoubleU <2wheeledwhitfield@gmail.com>
2019-04-25 02:27:47 +0100

    Stramlined backend python
    Multithreading reduces time taken by ~70%. Additionally DB commit has been moved for repeated minimal commits reducing the chance of a lock on the DB considerably.

--- RSS.py
+++ RSS.py
@@ -8,24 +8,23 @@ import re
 import time
 import datetime
 import html
+import multiprocessing
 
 def get_image(source): 
 	backup = re.search('src="(http(s)?://.*?)"', source.summary)
 	if "media_thumbnail" in source:
 		return source["media_thumbnail"][0]["url"]
 	elif "media_content" in source:
-		return source["media_content"][0]["url"]
+		return source["media_content"][0]["url"] #fix non images being returned here - do not assume media is image vs mp3 etc
 	elif backup:
 		return html.unescape(backup.group(1))
 	else:
 		return "noImage.png"
-		#add proper image for when feed has no image
+		#to be properly fleshed out once other concerns are taken care of
 
 def get_time(source):
 	if "published_parsed" in source:
-		# logging comment to remove return 
-		#time.strftime('%Y-%m-%dT%H:%M:%SZ', article['published_parsed'])
-		return datetime.datetime.utcnow().isoformat()+"Z"
+		return datetime.datetime.utcnow().isoformat()+"Z" #extraneous currently, but not yet decided if feeds date should be trusted - remove if not
 	else:
 		return datetime.datetime.utcnow().isoformat()+"Z"
 
@@ -40,30 +39,25 @@ db_connection.commit()
 #db.executemany("INSERT INTO feeds VALUES (?,?,?)", records)
 #db_connection.commit()
 
-while True:
-
-	db.execute('SELECT * from feeds')
-
-	for feed in db.fetchall():
+def feeder(feed):
 	    feed_results = feedparser.parse(feed[2])
-	    #logging
-	    #print(feed[0])
 	    for article in feed_results['entries']:
 	    	if ('id' in article):
 	    		article_id = (article['id'])
 	    	else:
-	    		article_id = article['link']
-	    	db.execute('SELECT count(*) from article_entries WHERE uid=?', [article_id])
+	    		article_id = article['link'] #this should be fine unless for some reason links are duplicated (which in an RSS feed lacking IDs could well be the case, but one can only do so much)
+	    	db.execute('SELECT count(*) from article_entries WHERE uid=?', [article_id]) 
 	    	if (not db.fetchone()[0]) and ('link' in article):
-	        	print(article['title'])
 	        	image_link = get_image(article)
 	        	times = get_time(article)
 	        	db.executemany('INSERT INTO article_entries VALUES (?,?,?,?,?,?,?,?)', [(article_id,article['title'],feed[1],feed[0],article['link'],image_link,times,0)])
-	        	#logging
-	        	#print("added article", article['title'], "to category", feed[1])
-	db_connection.commit()
-	print("commit")
-	time.sleep(300)
-	print("going again")
+	        	db_connection.commit() #commit shifted to allow parallelisation and fix (mostly) any DB locking issues
 
-#This entire thing works but could do with polish and then needs daemonising
+pool = multiprocessing.Pool(4)  #CPU bound on 4 core 
+ 
+while True:
+	db.execute('SELECT * from feeds')
+	pool.map(feeder, db, chunksize=4) #from testing 3/4 gives best performance
+	time.sleep(40)
+
+#partial rewrite - polish and then needs daemonising tbd
